a6cf5c6f72571007e3eda97e5c0f4887b39de17d
RustyYato <rustyyato@gmail.com>
2024-05-05 13:03:16 -0700

    make it build everything

--- Algebra.lean
+++ Algebra.lean
@@ -1,3 +1,4 @@
 -- This module serves as the root of the `Algebra` library.
 -- Import modules here that should be built as part of the library.
-import «Algebra».Basic
+import «Algebra».Nat.Prime
+import «Algebra».SortedList.Basic
