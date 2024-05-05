a0fd03984907ab44b1c56698d92e3c9c66f5010e
RustyYato <rustyyato@gmail.com>
2024-05-04 18:58:56 -0700

    link dvd and div

--- Algebra/Nat/Dvd.lean
+++ Algebra/Nat/Dvd.lean
@@ -17,6 +17,23 @@ def nat.one_dvd (b: nat) : 1 ∣ b := by
 
 #print axioms nat.one_dvd
 
+def nat.dvd_refl (a: nat) : a ∣ a := by
+  exists 1
+  rw [mul_one]
+
+#print axioms nat.dvd_refl
+
+def nat.dvd_mul_left (a b: nat) : a ∣ a * b := by
+  exists b
+
+#print axioms nat.dvd_mul_left
+
+def nat.dvd_mul_right (a b: nat) : a ∣ b * a := by
+  exists b
+  rw [mul_comm]
+
+#print axioms nat.dvd_mul_right
+
 def nat.eq_zero_of_zero_dvd {b: nat} : 0 ∣ b -> b = 0 := by
   match b with
   | .zero => intro; rfl
@@ -97,4 +114,100 @@ instance nat.dvd_dec (a b: nat) : Decidable (a ∣ b) := by
     contradiction
 
 #print axioms nat.dvd_dec
+
+def nat.dvd.le { a b: nat } ( b_nz: 0 < b ) (a_dvd_b: a ∣ b) : a ≤ b := by
+  match b with
+  | .succ b =>
+  have ⟨ k, prf ⟩ := a_dvd_b
+  clear b_nz
+  match k with
+  | .zero =>
+    rw [zero_eq, mul_zero] at prf
+    contradiction
+  | .succ k =>
+  match a with
+  | .zero =>
+    rw [zero_eq, zero_mul] at prf
+    contradiction
+  | .succ a =>
+    have := mul_ge a.succ k.succ rfl
+    rw [prf] at this
+    exact this
+
+#print axioms nat.dvd.le
+
+def nat.dvd_antisymm { a b: nat } : a ∣ b -> b ∣ a -> a = b := by
+  intro a_dvd_b b_dvd_a
+  match b with
+  | .zero =>
+    have := eq_zero_of_zero_dvd b_dvd_a
+    rw [this]
+    rfl
+  | .succ b =>
+  match a with
+  | .zero => 
+    have := eq_zero_of_zero_dvd a_dvd_b
+    rw [this]
+    rfl
+  | .succ a =>
+  apply le_antisymm
+  exact nat.dvd.le rfl a_dvd_b
+  exact nat.dvd.le rfl b_dvd_a
+
+#print axioms nat.dvd_antisymm
+
+def nat.dvd_sub { a b: nat } : a ≤ b -> a ∣ b -> a ∣ (b - a) := by
+  intro a_le_b a_dvd_b
+  have ⟨ k, prf ⟩ := a_dvd_b
+  match k with
+  | .zero =>
+    rw [zero_eq, mul_zero] at prf
+    rw [←prf] at a_dvd_b a_le_b
+    rw [←prf]
+    rw [le_zero a_le_b]
+    exists 0
+  | .succ k =>
+    exists k
+    rw [←prf]
+    rw [mul_succ, add_comm, ←add_sub, sub_refl, add_zero]
+    exact le_refl _
+
+def nat.dvd_def { a b: nat } : b ∣ a -> a = (a / b) * b := by
+  match b with
+  | .zero =>
+    intro zero_dvd_a
+    rw [eq_zero_of_zero_dvd zero_dvd_a]
+    rfl
+  | .succ b =>
+  have : ∀(a b: nat), b ∣ a -> 0 < b -> a = (a / b) * b := by
+    intro a b q r
+    apply nat.div_mod_induction (fun a b => b ∣ a -> 0 < b -> a = (a / b) * b)
+    {
+      intro a b a_lt_b b_dvd_a b_nz
+      rw [nat.div.is_lt']
+      rw [zero_mul]
+      match a with
+      | .zero => rfl
+      | .succ a =>
+        have b_le_a_succ := nat.dvd.le rfl b_dvd_a
+        have := not_lt_and_ge a_lt_b b_le_a_succ
+        contradiction
+      repeat assumption
+    }
+    {
+      intro a b a_ge_b ih b_dvd_a b_nz
+      rw [nat.div.is_ge', succ_mul, ←ih, add_sub, add_comm, ←add_sub, sub_refl, add_zero]
+      apply le_refl
+      any_goals assumption
+      apply nat.dvd_sub
+      repeat assumption
+
+    }
+    repeat assumption
+  intro b_dvd_a
+  apply this
+  assumption
+  rfl
+
+#print axioms nat.dvd_def
 
