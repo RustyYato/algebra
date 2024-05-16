6ae0d85c3d0baa52226824c89db74c58fb4f85e1
RustyYato <rustyyato@gmail.com>
2024-05-15 20:36:12 -0700

    prove that first factor of a number is prime

--- Algebra/Nat/Prime.lean
+++ Algebra/Nat/Prime.lean
@@ -119,3 +119,169 @@ instance nat.prime.instDec (a: nat) : Decidable a.prime := by
 
 #print axioms nat.prime.instDec
 
+def nat.first_factor.find (a: nat) (current: nat) (fuel: nat) : nat := 
+  match fuel with
+  | .zero => current
+  | .succ fuel => 
+    match (inferInstance : Decidable (current ∣ a)) with
+    | .isTrue h => current
+    | .isFalse _ => find a current.succ fuel
+
+def nat.first_factor (a: nat) : nat := first_factor.find a 2 (a - 2)
+
+#print axioms nat.first_factor
+
+def nat.first_factor.find.is_factor (a current fuel: nat) : a = fuel + current -> nat.first_factor.find a current fuel ∣ a := by
+  induction fuel generalizing current with
+  | zero =>
+    intro enough_fuel
+    rw [zero_eq, zero_add] at enough_fuel
+    unfold first_factor.find
+    rw [enough_fuel]
+    apply nat.dvd_refl
+  | succ fuel ih => 
+    intros enough_fuel
+    unfold first_factor.find
+    split
+    assumption
+    apply ih
+    rw [add_succ, ←succ_add]
+    assumption
+
+def nat.first_factor.is_factor (a: nat) : 1 < a -> a.first_factor ∣ a := by
+  intro one_lt_a
+  match a with
+  | 0 | 1 => contradiction
+  | .succ (.succ _) => 
+  apply nat.first_factor.find.is_factor
+  have : 2 = nat.zero.succ.succ := rfl
+  rw [this, succ_sub_succ, succ_sub_succ, zero_eq, sub_zero]
+  rw [add_succ, add_succ, add_zero]
+
+def nat.first_factor.find.lower_bound (a current fuel: nat) :
+  current ≤ nat.first_factor.find a current fuel := by
+  induction fuel generalizing current with
+  | zero => 
+    apply le_refl
+  | succ fuel ih =>
+    unfold find
+    split 
+    apply le_refl
+    apply TotalOrder.le_trans _ (ih current.succ)
+    apply TotalOrder.le_of_lt
+    apply nat.lt_succ_self
+
+def nat.first_factor.find.is_smallest_factor (a current fuel: nat) :
+  a = fuel + current ->
+  (∀k, 1 < k -> k < current -> ¬ k ∣ a) ->
+  (∀k, 1 < k -> k ∣ a -> nat.first_factor.find a current fuel ≤ k) := by
+  induction fuel generalizing current with
+  | zero =>
+    intro enough_fuel prev k k_gt_1 k_dvd_current
+    rw [zero_eq, zero_add] at enough_fuel
+    rw [enough_fuel] at k_dvd_current prev
+    match current with
+    | .succ current => 
+      cases TotalOrder.lt_or_eq_of_le (nat.dvd.le nat.zero_lt_succ k_dvd_current) with
+      | inl h =>
+        have := prev k k_gt_1 h 
+        contradiction
+      | inr h =>
+        unfold find
+        rw [h]
+        apply le_refl
+    | .zero => 
+    apply zero_le
+  | succ fuel ih =>
+    intro enough_fuel prev k k_gt_1 k_dvd_a
+    unfold find
+    split
+    {
+      rename_i current_dvd_a _
+      cases (inferInstance: Decidable (current ≤ k)) with
+      | isTrue => assumption
+      | isFalse h =>
+        have k_lt_current := TotalOrder.not_ge_implies_lt h
+        apply False.elim
+        have := prev k k_gt_1 k_lt_current
+        contradiction
+    }
+    {
+      apply ih
+      {
+        rw [add_succ, ←succ_add]
+        assumption
+      }
+      {
+        intro b b_gt_1 b_lt_succ_current
+        cases TotalOrder.lt_or_eq_of_le <| nat.le_of_lt_succ b_lt_succ_current with
+        | inr b_eq_current =>
+          rw [b_eq_current]
+          assumption
+        | inl b_eq__current => 
+          apply prev 
+          any_goals assumption
+      }
+      assumption
+      assumption
+    }
+
+
+def nat.first_factor.is_smallest_factor (a: nat) : 1 < a -> ∀k, 1 < k -> k ∣ a -> a.first_factor ≤ k := by
+  intro one_lt_a k k_gt_1 k_dvd_a
+  apply nat.first_factor.find.is_smallest_factor
+  {
+    match a with
+    | .succ (.succ a) =>
+    have : 2 = nat.zero.succ.succ := rfl
+    rw [this, succ_sub_succ, succ_sub_succ, zero_eq, sub_zero, add_succ, add_succ, add_zero]
+  }
+  {
+    intro b b_gt_one b_lt_two
+    match b with
+    | 0 | 1 => contradiction
+    | .succ (.succ b) =>
+      have := nat.not_lt_zero b_lt_two
+      contradiction
+  }
+  repeat assumption
+
+def nat.prime.first_factor.is_prime (a: nat) : 1 < a -> a.first_factor.prime := by
+  intro one_lt_a
+  have is_factor := nat.first_factor.is_factor a one_lt_a
+  have smallest := nat.first_factor.is_smallest_factor a one_lt_a
+  apply And.intro
+  unfold first_factor
+  have := nat.first_factor.find.lower_bound a 2 (a - 2)
+  apply TotalOrder.lt_of_lt_and_le
+  apply nat.lt_succ_self
+  exact this
+  intro k k_dvd_fst
+  have k_dvd_a := nat.dvd.trans k_dvd_fst is_factor
+  match k with
+  | 0 =>
+    apply Or.inr
+    apply Eq.symm
+    apply nat.eq_zero_of_zero_dvd
+    assumption
+  | 1 => exact Or.inl rfl
+  | .succ (.succ k) => 
+  have := smallest k.succ.succ (by
+    apply nat.succ_lt_succ
+    exact nat.zero_lt_succ) k_dvd_a
+  apply Or.inr
+  apply TotalOrder.le_antisymm
+  apply nat.dvd.le _ k_dvd_fst
+  {
+    cases (inferInstance: Decidable (0 < first_factor a)) with
+    | isTrue => assumption
+    | isFalse h =>
+      have := TotalOrder.not_lt_implies_ge h
+      rw [nat.le_zero this] at is_factor
+      rw [nat.eq_zero_of_zero_dvd is_factor] at one_lt_a
+      contradiction
+  }
+  assumption
+
+#print axioms nat.prime.first_factor.is_prime
+
