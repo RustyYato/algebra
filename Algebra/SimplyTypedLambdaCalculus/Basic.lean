caf9107c2891569a2cc77acf10298595d550af65
RustyYato <rustyyato@gmail.com>
2024-06-27 14:21:45 -0700

    prove that reduction steps are subsingleton and determanistic

--- Algebra/SimplyTypedLambdaCalculus/Basic.lean
+++ Algebra/SimplyTypedLambdaCalculus/Basic.lean
@@ -29,7 +29,7 @@ inductive Term : TypeCtx n -> LamTy -> Type where
 def Term.is_terminal (term: Term ctx ty) : Prop :=
   match term with
   | .Unit => True
-  | .Var _ _ (.refl _) => True
+  | .Var _ _ _ => True
   | .Abort t _ => t.is_terminal
   | .Lam _ _ body => body.is_terminal
   | .App _ _ func arg =>
@@ -178,87 +178,187 @@ decreasing_by
 
 #print axioms Term.subst_at
 
--- inductive Subst :
---   ∀{ n: nat } { ctx: TypeCtx n.succ } (pos: fin n.succ) (subst_ty ty: LamTy),
---   -- the substituted value should have type `subst_ty` in the context where
---   -- the substituted variable at pos is removed
---   (subst: Term (ctx.remove pos) subst_ty) ->
---   -- the term before the substitution
---   (before: Term ctx ty) ->
---   -- the term after the substitution
---   -- note that it must have the same type
---   -- as before the substitution and in the
---   -- context where the variable at pos is removed
---   (after: Term (ctx.remove pos) ty) -> Type where
---   | Unit : Subst pos subst_ty .Unit subst .Unit .Unit
---   | VarEq : Subst pos subst_ty subst_ty subst (.Var pos _ refl) subst
---   | VarLt :
---     ∀{n: nat} { ctx: TypeCtx n.succ } { pos idx: fin n.succ} {subst_ty: LamTy}
---       {subst: Term (ctx.remove pos) subst_ty},
---       (idx_lt_pos: idx < pos) ->
---     Subst pos subst_ty (ctx.get idx) subst (.Var idx (ctx.get idx) rfl) (by
---       apply Term.Var (fin.mk idx.val _)
---       rw [Vector.remove_get_lt]
---       conv => {
---         rhs
---         rhs
---         conv => {
---           lhs
---           rw [fin.mk_val _ (by
---             apply nat.lt_of_lt_and_le idx_lt_pos
---             apply nat.le_of_lt_succ
---             exact pos.valLt)]
---         }
---       }
---       rw [fin.val_mk]
---       rw [fin.mk_val]
---       assumption)
---   | VarGt :
---     ∀{n: nat} { ctx: TypeCtx n.succ } { pos idx: fin n.succ} {subst_ty: LamTy}
---       {subst: Term (ctx.remove pos) subst_ty},
---       (idx_gt_pos: idx > pos) ->
---     Subst pos subst_ty (ctx.get idx) subst (.Var idx (ctx.get idx) rfl) (by
---       apply Term.Var (fin.mk idx.val.pred _) _ _
---       {
---         cases idx
---         have := nat.not_lt_zero idx_gt_pos
---         contradiction
---         unfold fin.val nat.pred
---         dsimp
---         rename_i a; exact a.valLt
---       }
---       {
---         cases idx with
---         | zero =>
---           cases pos <;> contradiction
---         | succ idx =>
---         unfold fin.val nat.pred
---         dsimp
---         conv in fin.mk idx.val _ => {
---           rw [fin.val_mk]
---         }
---         rw [Vector.remove_get_ge]
---         apply nat.le_of_lt_succ
---         exact idx_gt_pos
---     })
---   | Abort :
---       Subst pos subst_ty .Void subst body new_body ->
---       Subst pos subst_ty ty susbt (.Abort body ty) (.Abort new_body ty)
---   | Func :
---       ∀{ arg_ty ret_ty subst_ty: LamTy }
---       { body: Term (.cons arg_ty ctx) ret_ty }
---       { new_body: Term (.cons arg_ty ctx) ret_ty }
---       { subst: Term (ctx.remove pos) subst_ty },
---       Subst pos.succ subst_ty ret_ty (by
---         unfold Vector.remove
---         simp
---         exact subst
---         admit) body new_body ->
---       Subst pos subst_ty (.Func arg_ty ret_ty) subst (.Lam arg_ty ret_ty body) (.Lam arg_ty ret_ty new_body)
-
-
--- #print axioms Subst
+def Term.subst
+  { n: nat }
+  { ctx: TypeCtx n.succ }
+  (term: Term (Vector.cons arg_ty ctx) ty)
+  (subst: Term ctx arg_ty) : Term ctx ty :=
+  Term.subst_at term .zero subst
 
 inductive ReductionStep : Term ctx ty -> Term ctx ty -> Type where
 | Abort : ∀(body body': Term ctx .Void) ty, ReductionStep body body' -> ReductionStep (.Abort body ty) (.Abort body' ty)
---| Abort : ∀(body body': Term ctx .Void) ty, ReductionStep body body' -> ReductionStep (.Abort body ty) (.Abort body' ty)
+| LamBody :
+  ∀arg_ty ret_ty (body body': Term (Vector.cons arg_ty ctx) ret_ty), ReductionStep body body' -> ReductionStep (.Lam arg_ty ret_ty body) (.Lam arg_ty ret_ty body')
+| AppFunc :
+  ∀arg_ty ret_ty (func func': Term ctx (.Func arg_ty ret_ty)) (arg: Term ctx arg_ty), ReductionStep func func'
+    -> ReductionStep (.App arg_ty ret_ty func arg) (.App arg_ty ret_ty func' arg)
+| AppArg :
+  ∀arg_ty ret_ty (func: Term ctx (.Func arg_ty ret_ty)) (arg arg': Term ctx arg_ty),
+    func.is_terminal ->
+    ReductionStep arg arg'
+    -> ReductionStep (.App arg_ty ret_ty func arg) (.App arg_ty ret_ty func arg')
+| Apply :
+  ∀arg_ty ret_ty (body: Term (Vector.cons arg_ty ctx) ret_ty) (arg: Term ctx arg_ty) (output: Term ctx ret_ty),
+    arg.is_terminal ->
+    body.is_terminal ->
+    output = body.subst arg ->
+    ReductionStep (.App arg_ty ret_ty (.Lam arg_ty ret_ty body) arg) output
+
+infix:60 " ⤳ " => ReductionStep
+
+inductive ReductionStepList : Term ctx ty -> Term ctx ty -> Type where
+| Refl : ∀(a: Term ctx ty), a.is_terminal -> ReductionStepList a a
+| Cons : ∀{a b c}, a ⤳ b -> ReductionStepList b c ->  ReductionStepList a c
+
+infix:60 " ~~> " => ReductionStepList
+
+def ReductionStepList.length (red: ReductionStepList a b)  : nat :=
+  match red with
+  | .Refl _ _ => 0
+  | .Cons _ list => list.length
+
+instance ReductionStep.never_terminal:
+  a ⤳ b -> ¬a.is_terminal := by
+  intro red
+  induction red with
+  | Abort body body' ty red ih => exact ih
+  | LamBody arg_ty ret_ty body body'  red ih => exact ih
+  | AppFunc arg_ty ret_ty func func' arg red ih =>
+    intro t
+    apply ih
+    unfold Term.is_terminal at t
+    cases func
+    any_goals (have ⟨ _, _ ⟩ := t; assumption; done)
+    contradiction
+  | AppArg arg_ty ret_ty func arg arg' func_term red ih =>
+    intro t
+    apply ih
+    unfold Term.is_terminal at t
+    cases func
+    any_goals (have ⟨ _, _ ⟩ := t; assumption; done)
+    contradiction
+  | Apply arg_ty ret_ty body arg output arg_term body_term output_correct =>
+    unfold Term.is_terminal
+    dsimp
+    exact id
+
+def ReductionStep.determanistic : a ⤳ b -> a ⤳ c -> b = c := by
+  intro a_to_b a_to_c
+  induction a_to_b with
+  | Abort body body' ty red ih =>
+    cases a_to_c
+    congr
+    apply ih
+    assumption
+  | LamBody arg_ty ret_ty body body' red ih =>
+    cases a_to_c
+    congr
+    apply ih
+    assumption
+  | AppFunc arg_ty ret_ty func func' arg red ih =>
+    cases a_to_c
+    congr
+    apply ih
+    assumption
+    rename_i h
+    have := red.never_terminal
+    contradiction
+    have := red.never_terminal
+    contradiction
+  | AppArg arg_ty ret_ty func arg arg' func_term red ih =>
+    cases a_to_c
+    rename_i h
+    have := h.never_terminal
+    contradiction
+    congr
+    apply ih
+    assumption
+    have := red.never_terminal
+    contradiction
+  | Apply arg_ty ret_ty body arg output arg_term body_term out_def =>
+    cases a_to_c
+    rename_i h
+    have := h.never_terminal
+    contradiction
+    rename_i h
+    have := h.never_terminal
+    contradiction
+    subst c
+    subst output
+    rfl
+
+#print axioms ReductionStep.determanistic
+
+instance ReductionStep.subsingleton: Subsingleton (a ⤳ b) where
+  allEq := by
+    intro x y
+    induction x with
+    | Abort xbody xbody' xty xred xih =>
+      cases y
+      congr
+      apply xih
+    | LamBody arg_ty ret_ty body body' red ih =>
+      cases y
+      congr
+      apply ih
+    | AppFunc arg_ty ret_ty func func' arg red ih =>
+      cases y
+      congr
+      apply ih
+      have := red.never_terminal
+      contradiction
+      have := red.never_terminal
+      contradiction
+    | AppArg arg_ty ret_ty func arg arg' func_term red ih =>
+      cases y
+      rename_i red'
+      have := red'.never_terminal
+      contradiction
+      congr
+      apply ih
+      have := red.never_terminal
+      contradiction
+    | Apply arg_ty ret_ty body arg output arg_term body_term output_correct =>
+      cases y
+      rename_i body_red
+      have := body_red.never_terminal
+      contradiction
+      rename_i arg_red
+      have := arg_red.never_terminal
+      contradiction
+      congr
+
+#print axioms ReductionStep.subsingleton
+
+def ReductionStepList.subsingleton : Subsingleton (a ~~> b) where
+  allEq := by
+    intro x y
+    induction x with
+    | Refl x x_term =>
+      cases y
+      rfl
+      rename_i h _
+      have := h.never_terminal
+      contradiction
+    | Cons x xs ih =>
+      cases y
+      have := x.never_terminal
+      contradiction
+      rename_i x₀ x₁ x₂ y₀ x₀_to_y₀ y₀_to_x₂
+      have := x.determanistic x₀_to_y₀
+      subst y₀
+      congr
+      apply Subsingleton.allEq
+      apply ih
+
+#print axioms ReductionStepList.subsingleton
+
+def ReductionStep.allHEq
+  (x: a ⤳ b) (y: a ⤳ c):
+  HEq x y := by
+  have := x.determanistic y
+  subst c
+  congr
+  apply Subsingleton.allEq
+
+#print axioms ReductionStep.allHEq
