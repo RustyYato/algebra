fea205b8485a17b4381fe46a4e5b2f20d4830145
RustyYato <rustyyato@gmail.com>
2024-10-12 18:43:16 -0700

    setup real numbers

--- Algebra/Real/Basic.lean
+++ Algebra/Real/Basic.lean
@@ -74,3 +74,15 @@ def CauchySeq.Equivalence : Equivalence Equiv where
 instance CauchySeq.setoid : Setoid CauchySeq where
   r := Equiv
   iseqv := CauchySeq.Equivalence
+
+def real := Quotient CauchySeq.setoid
+
+def real.mk : CauchySeq -> real := Quotient.mk CauchySeq.setoid
+def real.ind : { motive: real -> Prop } -> (seq: ∀s, motive (mk s)) -> ∀r, motive r := Quotient.ind
+def real.lift : (f: CauchySeq -> α) -> (∀a b, a ≈ b -> f a = f b) -> real -> α := Quotient.lift
+def real.lift₂ : (f: CauchySeq -> CauchySeq -> α) -> (∀a b c d, a ≈ c -> b ≈ d -> f a b = f c d) -> real -> real -> α := Quotient.lift₂
+def real.lift_mk : lift f all_eq (mk a) = f a := rfl
+def real.lift₂_mk : lift₂ f all_eq (mk a) (mk b) = f a b := rfl
+def real.exact : mk a = mk b -> a ≈ b := Quotient.exact
+def real.sound : a ≈ b -> mk a = mk b := Quotient.sound
+def real.exists_rep : ∀r, ∃c, mk c = r := Quotient.exists_rep
