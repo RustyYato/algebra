474c377ff68efc979bbbeffc62c4798818104f84
RustyYato <rustyyato@gmail.com>
2024-11-18 16:20:36 -0700

    update to avoid sublist

--- Algebra/LambdaCalc/SystemF.lean
+++ Algebra/LambdaCalc/SystemF.lean
@@ -18,6 +18,7 @@ abbrev TypeCtx := Nat
 abbrev TermCtx := List LamType
 
 inductive LamType.IsWellFormed : TypeCtx -> LamType -> Prop where
+| Void : IsWellFormed n .Void
 | Var n m : m < n -> IsWellFormed n (.Var m)
 | Fn n arg ret : IsWellFormed n arg -> IsWellFormed n ret -> IsWellFormed n (.Fn arg ret)
 | Forall n body : IsWellFormed n.succ body -> IsWellFormed n (.Forall body)
@@ -30,10 +31,11 @@ def LamType.weakenAtBy (ty: LamType) (n m: Nat) : LamType :=
   match ty with
   | .Void => .Void
   | .Var x =>
+    .Var <|
     if x < n then
-      .Var x
+      x
     else
-      .Var (x + n + m)
+      x + n + m
   | .Fn arg ret => .Fn (arg.weakenAtBy n m) (ret.weakenAtBy n m)
   | .Forall body => .Forall (body.weakenAtBy n.succ m)
 
@@ -84,16 +86,22 @@ def Term.substAt (term subst: Term) (type_n term_n: Nat) : Term :=
   | .TypeLam body => .TypeLam (body.substAt subst type_n.succ term_n)
   | .Panic body ty => .Panic (body.substAt subst type_n term_n) ty
 
+def LamType.subst (term subst: LamType) := term.substAt subst 0
+def Term.subst (term subst: Term) := term.substAt subst 0 0
+
+def TermCtx.weakenBy (ctx: TermCtx) (n: Nat) : TermCtx := ctx.map (·.weakenBy n)
+
 inductive Term.IsWellTyped : TypeCtx -> TermCtx -> Term -> LamType -> Prop where
 | Var tyctx ctx term_binders (h: term_binders < ctx.length) ty :
   ty = ctx[term_binders] ->
   IsWellTyped tyctx ctx (.Var term_binders) ty
 | TermLam tyctx ctx arg_ty ret_ty body :
+  arg_ty.IsWellFormed tyctx ->
   IsWellTyped tyctx (arg_ty::ctx) body ret_ty ->
   IsWellTyped tyctx ctx (.TermLam arg_ty body) (.Fn arg_ty ret_ty)
 | TypeLam tyctx ctx ret_ty body :
-  IsWellTyped tyctx.succ ctx body ret_ty ->
-  IsWellTyped tyctx ctx (.TermLam arg_ty body) (.Forall ret_ty)
+  IsWellTyped tyctx.succ (ctx.weakenBy 1) body ret_ty ->
+  IsWellTyped tyctx ctx (.TypeLam body) (.Forall ret_ty)
 | TermApp tyctx ctx f arg :
   IsWellTyped tyctx ctx f (.Fn arg_ty ret_ty) ->
   IsWellTyped tyctx ctx arg arg_ty ->
@@ -101,9 +109,179 @@ inductive Term.IsWellTyped : TypeCtx -> TermCtx -> Term -> LamType -> Prop where
 | TypeApp tyctx ctx f arg :
   IsWellTyped tyctx ctx f (.Forall ret_ty) ->
   arg.IsWellFormed tyctx ->
-  ret_ty' = ret_ty.substAt arg 0 ->
+  ret_ty' = ret_ty.subst arg ->
   IsWellTyped tyctx ctx (.TypeApp f arg) ret_ty'
 | Panic body ty :
   IsWellTyped tyctx ctx body .Void ->
   ty.IsWellFormed tyctx ->
   IsWellTyped tyctx ctx (.Panic body ty) ty
+
+def TermCtx.IsWellFormed.get (ctx: TermCtx) :
+  ctx.IsWellFormed tyctx ->
+  ∀x (h: x < ctx.length),
+  ctx[x].IsWellFormed tyctx := by
+  intro ctxwf x h
+  induction ctxwf generalizing x with
+  | nil => contradiction
+  | cons ty ctx tywf ctxwf ih =>
+    cases x with
+    | zero => assumption
+    | succ x => apply ih
+
+def LamType.IsWellFormed.weakenAtBy.add (ty: LamType) :
+  (ty.weakenAtBy pos x).IsWellFormed tyctx ->
+  (ty.weakenAtBy (pos + pos') (x + x')).IsWellFormed (tyctx + pos' + x') := by
+  intro tywf
+  induction ty generalizing tyctx pos x pos' x' with
+  | Void => apply IsWellFormed.Void
+  | Fn arg ret argih retih =>
+    cases tywf
+    apply IsWellFormed.Fn
+    apply argih <;> assumption
+    apply retih <;> assumption
+  | Forall body ih =>
+    cases tywf with | Forall _ _ h =>
+    apply IsWellFormed.Forall
+    repeat rw [←Nat.succ_add]
+    apply ih
+    assumption
+  | Var n =>
+    cases tywf with | Var _ _ h =>
+    apply IsWellFormed.Var
+    split at h
+    rename_i n_lt_pos
+    rw [if_pos, Nat.add_assoc]
+    apply Nat.lt_add_right
+    assumption
+    apply Nat.lt_add_right
+    assumption
+    rename_i pos_le_n
+    replace pos_le_n := Nat.le_of_not_lt pos_le_n
+    split
+    rw [Nat.add_assoc]
+    apply Nat.lt_add_right
+    rw [Nat.add_assoc] at h
+    apply Nat.lt_of_add_right_lt
+    assumption
+    repeat rw [Nat.add_assoc]
+    rw [Nat.add_left_comm pos']
+    rw [←Nat.add_assoc n, ←Nat.add_assoc (n + _)]
+    apply Nat.add_lt_add_right
+    assumption
+
+def LamType.IsWellFormed.weakenBy.succ (ty: LamType) :
+  (ty.weakenBy x).IsWellFormed tyctx ->
+  (ty.weakenBy x.succ).IsWellFormed tyctx.succ :=
+    LamType.IsWellFormed.weakenAtBy.add (pos' := 0) (x' := 1) ty
+
+def LamType.IsWellFormed.weakenAtBy.add' (ty: LamType) :
+  ty.IsWellFormed tyctx ->
+  (ty.weakenAtBy n m).IsWellFormed (tyctx + n + m) := by
+  intro wf
+  induction ty generalizing n m tyctx with
+  | Void => apply IsWellFormed.Void
+  | Fn arg ret argih retih =>
+    cases wf
+    apply IsWellFormed.Fn
+    apply argih; assumption
+    apply retih; assumption
+  | Forall body ih =>
+    apply IsWellFormed.Forall
+    cases wf
+    rw [←Nat.succ_add]
+
+    apply ih
+    assumption
+  | Var x =>
+    apply IsWellFormed.Var
+    cases wf with | Var _ _ x_lt_tyctx =>
+    split
+    apply Nat.lt_add_right
+    apply Nat.lt_add_right
+    assumption
+    rename_i n_le_x
+    replace n_le_x := Nat.le_of_not_lt n_le_x
+    apply Nat.add_lt_add_right
+    apply Nat.add_lt_add_right
+    assumption
+
+def LamType.IsWellFormed.weakenBy.add (ty: LamType) :
+  ty.IsWellFormed tyctx ->
+  (ty.weakenBy n).IsWellFormed (tyctx + n) := by
+  intro wf
+  exact weakenAtBy.add' (n := 0) (m := n) _ wf
+
+def LamType.IsWellFormed.subst {ty subst: LamType} :
+  x ≤ tyctx ->
+  ty.IsWellFormed tyctx.succ ->
+  (subst.weakenBy x).IsWellFormed tyctx ->
+  (ty.substAt subst x).IsWellFormed tyctx := by
+  intro x_in_bounds tywf substwf
+  induction ty generalizing tyctx x with
+  | Void => apply IsWellFormed.Void
+  | Var n =>
+    cases tywf with | Var _ _ n_le_tyctx =>
+    unfold substAt
+    split <;> rename_i h
+    apply IsWellFormed.Var
+    apply Nat.lt_of_lt_of_le <;> assumption
+    replace h := Nat.le_of_not_lt h
+    rcases Nat.lt_or_eq_of_le h with x_lt_n | x_eq_n
+    rw [if_neg (Nat.ne_of_gt x_lt_n)]
+    apply IsWellFormed.Var
+    cases n
+    contradiction
+    apply Nat.lt_of_succ_lt_succ
+    assumption
+    rw [if_pos x_eq_n.symm]
+    assumption
+  | Fn arg ret argih retih =>
+    cases tywf with | Fn _ _ _ argwf retwf =>
+    apply IsWellFormed.Fn
+    apply argih <;> assumption
+    apply retih <;> assumption
+  | Forall body ih =>
+    cases tywf with | Forall _ _ bodywf =>
+    apply IsWellFormed.Forall
+    apply ih
+    apply Nat.succ_le_succ
+    assumption
+    assumption
+    apply LamType.IsWellFormed.weakenBy.succ
+    assumption
+
+def TermCtx.IsWellFormed.weakenBy (ctx: TermCtx) :
+  ctx.IsWellFormed tyctx ->
+  (ctx.weakenBy n).IsWellFormed (tyctx + n) := by
+  intro ctxwf
+  induction ctxwf with
+  | nil => apply IsWellFormed.nil
+  | cons ty ctx tywf ctxwf ih =>
+    apply IsWellFormed.cons
+    dsimp
+    apply LamType.IsWellFormed.weakenBy.add
+    assumption
+    assumption
+
+def Term.IsWellTyped.TypeIsWellFormed (term: Term) :
+  ctx.IsWellFormed tyctx ->
+  term.IsWellTyped tyctx ctx ty ->
+  ty.IsWellFormed tyctx := by
+  intro ctxwf termwt
+  induction termwt with
+  | Var tyctx ctx x x_in_bounds ty =>
+    subst ty
+    apply ctxwf.get
+  | TermLam tyctx ctx arg_ty ret_ty body arg_tywf bodywt ih =>
+    apply LamType.IsWellFormed.Fn
+    assumption
+    apply ih
+    apply ctxwf.cons
+    assumption
+  | TypeLam tyctx ctx ret_ty body body_wt ih  =>
+    apply LamType.IsWellFormed.Forall
+    apply ih
+    apply ctxwf.weaken
+  | TermApp => sorry
+  | TypeApp => sorry
+  | Panic => sorry
--- Algebra/ProjectivePlane/Basic.lean
+++ Algebra/ProjectivePlane/Basic.lean
@@ -20,14 +20,22 @@ instance Nat.decidableExistsFin (P: Fin n -> Prop) [d: DecidablePred P] : Decida
       apply h
       exact ⟨⟨val, Nat.lt_of_succ_lt_succ valLt⟩, Px⟩
 
+abbrev IsProjectivePlane.is_nondegen (incident: P -> L -> Prop) (l: L) (a b c: P) :=
+  incident a l -> incident b l -> incident c l -> False
+
+open IsProjectivePlane in
 class IsProjectivePlane {P L: Type*} (incident: P -> L -> Prop): Prop where
   unique_line: ∀a b: P, a ≠ b ->
     ∃l, incident a l ∧ incident b l ∧ ∀l', incident a l' -> incident b l' -> l = l'
   unique_point: ∀a b: L, a ≠ b ->
     ∃p, incident p a ∧ incident p b ∧ ∀p', incident p' a -> incident p' b -> p = p'
   nondegen: ∃a b c d: P, [a, b, c, d].Nodup ∧
-    ∀l a' b' c', [a', b', c'].Sublist [a, b, c, d] -> incident a' l -> incident b' l -> incident c' l -> False
+    ∀l, is_nondegen incident l a b c
+      ∧ is_nondegen incident l a b d
+      ∧ is_nondegen incident l a c d
+      ∧ is_nondegen incident l b c d
 
+open IsProjectivePlane in
 structure ProjectivePlane (P L: Type*) where
   -- is a point on a line
   incident: P -> L -> Prop
@@ -36,31 +44,26 @@ structure ProjectivePlane (P L: Type*) where
   unique_point: ∀a b: L, a ≠ b ->
     ∃p, incident p a ∧ incident p b ∧ ∀p', incident p' a -> incident p' b -> p = p'
   nondegen: ∃a b c d: P, [a, b, c, d].Nodup ∧
-    ∀l a' b' c', [a', b', c'].Sublist [a, b, c, d] -> incident a' l -> incident b' l -> incident c' l -> False
+    ∀l, is_nondegen incident l a b c
+      ∧ is_nondegen incident l a b d
+      ∧ is_nondegen incident l a c d
+      ∧ is_nondegen incident l b c d
+
+def ProjectivePlane.IsProjectivePlane (p: ProjectivePlane P L) :
+  IsProjectivePlane p.incident where
+  unique_line := p.unique_line
+  unique_point := p.unique_point
+  nondegen := p.nondegen
 
 def ProjectivePlane.fano_incident (a b: Fin 7) : Prop :=
   (a, b) ∈
- [(0, 0),
-  (1, 0),
-  (2, 0),
-  (2, 1),
-  (3, 1),
-  (4, 1),
-  (4, 2),
-  (5, 2),
-  (0, 2),
-  (0, 3),
-  (6, 3),
-  (3, 3),
-  (1, 4),
-  (6, 4),
-  (4, 4),
-  (2, 5),
-  (6, 5),
-  (5, 5),
-  (1, 6),
-  (3, 6),
-  (5, 6)]
+ [(0, 0), (1, 0), (2, 0),
+  (2, 1), (3, 1), (4, 1),
+  (4, 2), (5, 2), (0, 2),
+  (0, 3), (6, 3), (3, 3),
+  (1, 4), (6, 4), (4, 4),
+  (2, 5), (6, 5), (5, 5),
+  (1, 6), (3, 6), (5, 6)]
 
 instance ProjectivePlane.dec_fano_incident : Decidable (ProjectivePlane.fano_incident a b) := by
   unfold fano_incident
@@ -68,6 +71,15 @@ instance ProjectivePlane.dec_fano_incident : Decidable (ProjectivePlane.fano_inc
 
 def ProjectivePlane.FanoPlane : ProjectivePlane (Fin 7) (Fin 7) where
   incident := fano_incident
-  unique_line := by decide
-  unique_point := by decide
-  nondegen := by decide
+  unique_line := by decide +kernel
+  unique_point := by decide +kernel
+  nondegen := by decide +kernel
+
+def ProjectivePlane.fineq (p: ProjectivePlane (Fin P) (Fin L)) : P = L := by
+  have ⟨a,b,c,d,nodop,nondegen⟩  := p.nondegen
+  sorry
+
+def ProjectivePlane.BruckRyserChowla (p: ProjectivePlane (Fin P) (Fin L)) :
+  P % 4 = 1 ∨ P % 4 = 2 -> ∃x y, P = x * x + y * y := by
+  intro h
+  sorry
